e7824b3f7e616eecad877c86bdb14c4a58631f30
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-04-25 09:00:52 +0200

    init

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+
+public class ExampleTest {
+
+    @org.junit.jupiter.api.Test
+    public void exampleTest() {
+        // assert statements
+        Assertions.assertEquals(0, 10 * 0, "10 x 0 must be 0");
+    }
+}
