387e961e31f4167df0ee10e2f6c1e96c03f5b3d7
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-04-26 11:31:44 +0200

    Wyciąganie danych z wykorzystaniem Iteratora

--- src/main/java/kolekcje/NamesMap.java
+++ src/main/java/kolekcje/NamesMap.java
@@ -1,6 +1,7 @@
 package kolekcje;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class NamesMap {
@@ -11,14 +12,32 @@ public class NamesMap {
         printAllNames(namesMap);
 
         getNamesStartingAt(namesMap, "a");
+        getIndexesWithName(namesMap, "Jan");
     }
 
-    private static void getNamesStartingAt(Map<Long, String> namesMap, String startingPhrase) {
+    private static void getNamesStartingAt(Map<Long,
+            String> namesMap, String startingPhrase) {
         for (String name : namesMap.values()) {
             if (name.toLowerCase().startsWith(startingPhrase)) {
                 System.out.print(name + " ");
             }
         }
+        System.out.println();
+        System.out.println("------------------------");
+    }
+
+    private static void getIndexesWithName(Map<Long,
+            String> namesMap, String name) {
+        Iterator<Map.Entry<Long, String>> namesIterator =
+                namesMap.entrySet().iterator();
+        while (namesIterator.hasNext()) {
+            Map.Entry<Long, String> nameEntry = namesIterator.next();
+            if (name.equals(nameEntry.getValue())) {
+                System.out.print(nameEntry.getKey() + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("------------------------");
     }
 
     private static void populateMap(Map<Long, String> namesMap) {
@@ -27,6 +46,11 @@ public class NamesMap {
         namesMap.put(3L, "Antoni");
         namesMap.put(4L, "Henryk");
         namesMap.put(5L, "Elżbieta");
+        namesMap.put(6L, "Jan");
+        namesMap.put(7L, "Jan");
+        namesMap.put(8L, "Jan");
+        namesMap.put(9L, "Jan");
+        namesMap.put(10L, "Jan");
     }
 
     private static void printAllNames(Map<Long, String> namesMap) {
