96aa30b324555e8413de1c0d7ca2a626d32e37fa
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-05-10 10:08:37 +0200

    Callable

---
+++ src/main/java/watki/WorkerCallableRunner.java
@@ -0,0 +1,29 @@
+package watki;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class WorkerCallableRunner {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Future<String> future = executorService.submit(() -> {
+            Thread.sleep(5000);
+            return "Koniec";
+        });
+
+        while (!future.isDone()) {
+            System.out.println("Pracuję");
+            Thread.sleep(1000);
+        }
+
+        String result = future.get();
+        System.out.println(result);
+
+        executorService.shutdown();
+
+    }
+}
